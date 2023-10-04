7f6ecf918d86c55d64cd2e5a2aae74268e2c959b
Rita Tang <doggymint@gmail.com>
2023-10-04 18:29:45 -0400

    Move favButton state up to photolist

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,17 +1,11 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback } from "react";
 
 import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
-const PhotoFavButton = () => {
-  const [selected, setSelected] = useState(false);
-
-  const handleClick = () => {
-    setSelected(!selected);
-  };
-
+const PhotoFavButton = ({ selected, handleFavButtonClick }) => {
   return (
-    <div className="photo-list__fav-icon" onClick={handleClick}>
+    <div className="photo-list__fav-icon" onClick={handleFavButtonClick}>
       <div className="photo-list__fav-icon-svg">
         <FavIcon selected={selected} />
       </div>
