2f919ef077934aa52088284bbdb11fc240a2e82d
Rita Tang <doggymint@gmail.com>
2023-10-04 18:02:45 -0400

    Add state to store liked photos and pass down function to alter state to photo list

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavigationBar from "../components/TopNavigationBar";
 import PhotoList from "../components/PhotoList";
 import "../styles/HomeRoute.scss";
 
 const HomeRoute = () => {
+  const [favPhotos, setFavPhotos] = useState({});
+
+  // Include the photo in favPhotos when it is favorited
+  const addFavPhoto = (photo) => {
+    setFavPhotos((prev) => ({ ...prev, [photo.id]: { photo } }));
+  };
+
+  // Remove a photo from favPhotos when it is unfavorited
+  const removeFavPhoto = (photo) => {
+    setFavPhotos((prev) => {
+      delete prev[photo.id];
+      return prev;
+    });
+  };
+
   return (
     <div className="home-route">
       <TopNavigationBar />
-      <PhotoList />
+      <PhotoList addFavPhoto={addFavPhoto} removeFavPhoto={removeFavPhoto} />
     </div>
   );
 };
