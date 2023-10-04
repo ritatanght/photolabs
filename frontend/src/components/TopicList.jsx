298dc5a5f7cf90fa8318055fb60c7932decc8f0e
Rita Tang <doggymint@gmail.com>
2023-10-04 14:58:31 -0400

    Refractor to take in topics data fro mocks/

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,13 @@
 import React from "react";
+import topics from "../mocks/topics";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
 const TopicList = () => {
-  const topics = sampleDataForTopicList.map((topic) => (
+  const topicElements = topics.map((topic) => (
     <TopicListItem key={topic.id} {...topic} />
   ));
-  return <div className="top-nav-bar__topic-list">{topics}</div>;
+  return <div className="top-nav-bar__topic-list">{topicElements}</div>;
 };
 
 export default TopicList;
