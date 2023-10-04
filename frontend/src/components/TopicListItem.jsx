ebe43f32a56d53990a0fab3405f8a688f402fba2
Rita Tang <doggymint@gmail.com>
2023-10-04 12:42:42 -0400

    Revise to adapt to the data structure passed down from topic list

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,17 +2,11 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
-  const { label } = sampleDataForTopicListItem;
+const TopicListItem = (props) => {
+  const { title } = props;
   return (
     <div className="topic-list__item">
-      <span>{label}</span>
+      <span>{title}</span>
     </div>
   );
 };
