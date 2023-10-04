33ca58ce0f616d9bd1c72fa6bcfc276261bc8c59
Rita Tang <doggymint@gmail.com>
2023-10-04 18:10:50 -0400

    Pass addFavPhoto and removeFavPhoto func down to photo item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
+  const [selected, setSelected] = useState(false);
   const {
-    location: { city, country },
-    urls: { regular },
-    user: { name, profile },
+    photo: {
+      location: { city, country },
+      urls: { regular },
+      user: { name, profile },
+    },
+    addFavPhoto,
+    removeFavPhoto,
   } = props;
 
+  const handleFavButtonClick = (photoObj) => {
+    // Run addFavPhoto to include the current photo when it is clicked but not yet selected
+    // Otherwise, run the removeFavPhoto to remove the current photo from favorited photos
+    selected ? removeFavPhoto(photoObj) : addFavPhoto(photoObj);
+
+    // Toggle the favButton
+    setSelected(!selected);
+  };
   return (
     <li className="photo-list__item">
-      <PhotoFavButton />
+      <PhotoFavButton
+        handleFavButtonClick={() => handleFavButtonClick(props.photo)}
+        selected={selected}
+      />
       <img src={regular} className="photo-list__image" />
       <footer className="photo-list__user-details">
         <img src={profile} className="photo-list__user-profile" />
@@ -19,7 +35,6 @@ const PhotoListItem = (props) => {
           <span>{name}</span>
           <br />
           <span className="photo-list__user-location">
-            {" "}
             {`${city}, ${country}`}
           </span>
         </div>
