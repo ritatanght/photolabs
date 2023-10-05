60807adcb54f48c50303e1fbde676579d679b831
Rita Tang <doggymint@gmail.com>
2023-10-04 22:48:29 -0400

    Take in photo from props and console.log the photo

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 
-const PhotoDetailsModal = ({ setIsModalOpen }) => {
+const PhotoDetailsModal = ({ photo, setModal }) => {
+  console.log(photo);
   return (
     <div className="photo-details-modal">
       <button
         className="photo-details-modal__close-button"
-        onClick={() => setIsModalOpen(false)}
+        onClick={() => setModal({ isOpen: false })}
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
