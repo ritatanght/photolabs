37d68fb7d2cff590a8cf89d586276f1f02c3152c
Rita Tang <doggymint@gmail.com>
2023-10-05 14:03:44 -0400

    Insert jsx to display the larger version of the selected photo in modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoFavButton from "components/PhotoFavButton";
 
 const PhotoDetailsModal = ({ photo, setModal }) => {
-  console.log(photo);
+  const {
+    location: { city, country },
+    urls: { full },
+    user: { name, profile },
+  } = photo;
+
   return (
     <div className="photo-details-modal">
       <button
@@ -13,6 +19,32 @@ const PhotoDetailsModal = ({ photo, setModal }) => {
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      {/* Container for main large image */}
+      <section className="photo-details-modal__images">
+        <PhotoFavButton />
+        <img src={full} className="photo-details-modal__image" />
+
+        <footer className="photo-details-modal__photographer-details">
+          <img
+            src={profile}
+            className="photo-details-modal__photographer-profile"
+          />
+          <div className="photo-details-modal__photographer-info">
+            <span>{name}</span>
+            <br />
+            <span
+              className="photo-details-modal__photographer-location {
+                "
+            >
+              {`${city}, ${country}`}
+            </span>
+          </div>
+        </footer>
+      </section>
+      {/* Section for similar photos */}
+      <section className="photo-details-modal__images">
+        <h2 className="photo-details-modal__header">Similar Photos</h2>
+      </section>
     </div>
   );
 };
