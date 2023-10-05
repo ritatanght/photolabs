04d1852e8737f8cb83ca2cf3707cc8de9959753b
Rita Tang <doggymint@gmail.com>
2023-10-05 14:21:44 -0400

    Revise PhotoList to get photos from props if available

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PhotoListItem from "./PhotoListItem";
-import photos from "../mocks/photos";
+import photosData from "../mocks/photos";
 import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
+  const photos = props.photos || photosData;
   const photoElements = photos.map((photo) => (
     <PhotoListItem key={photo.id} photo={photo} {...props} />
   ));
