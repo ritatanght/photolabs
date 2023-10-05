8a9cfcd28acb8b27fb23711a82d8d7341f14b4d8
ritatanght <doggymint@gmail.com>
2023-10-05 15:15:25 -0400

    Move favPhotos up to App to pass down both routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,37 @@ import "./App.scss";
 
 const App = () => {
   const [modal, setModal] = useState({ isOpen: false });
+  const [favPhotos, setFavPhotos] = useState({});
+
+  // Include the photo in favPhotos when it is favorited
+  const addFavPhoto = (photo) => {
+    setFavPhotos((prev) => ({ ...prev, [photo.id]: { photo } }));
+  };
+
+  // Remove a photo from favPhotos when it is unfavorited
+  const removeFavPhoto = (photo) => {
+    setFavPhotos((prev) => {
+      const { [photo.id]: removed, ...remain } = prev;
+      return remain;
+    });
+  };
 
   return (
     <div className="App">
-      <HomeRoute setModal={setModal} />
+      <HomeRoute
+        setModal={setModal}
+        favPhotos={favPhotos}
+        addFavPhoto={addFavPhoto}
+        removeFavPhoto={removeFavPhoto}
+      />
       {modal.isOpen && (
-        <PhotoDetailsModal photo={modal.photo} setModal={setModal} />
+        <PhotoDetailsModal
+          photo={modal.photo}
+          setModal={setModal}
+          favPhotos={favPhotos}
+          addFavPhoto={addFavPhoto}
+          removeFavPhoto={removeFavPhoto}
+        />
       )}
     </div>
   );
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,28 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import TopNavigationBar from "../components/TopNavigationBar";
 import PhotoList from "../components/PhotoList";
 import "../styles/HomeRoute.scss";
 
-const HomeRoute = ({ setModal }) => {
-  const [favPhotos, setFavPhotos] = useState({});
-
-  // Include the photo in favPhotos when it is favorited
-  const addFavPhoto = (photo) => {
-    setFavPhotos((prev) => ({ ...prev, [photo.id]: { photo } }));
-  };
-
-  // Remove a photo from favPhotos when it is unfavorited
-  const removeFavPhoto = (photo) => {
-    setFavPhotos((prev) => {
-      const { [photo.id]: removed, ...remain } = prev;
-      return remain;
-    });
-  };
+const HomeRoute = (props) => {
+  const { favPhotos, addFavPhoto, removeFavPhoto, setModal } = props;
 
   return (
     <div className="home-route">
       <TopNavigationBar isFavPhotoExist={Object.keys(favPhotos).length > 0} />
       <PhotoList
+        favPhotos={favPhotos}
         addFavPhoto={addFavPhoto}
         removeFavPhoto={removeFavPhoto}
         setModal={setModal}
