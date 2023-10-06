1c7af46e27c1c66ad55f5cf68362c01ad5a47919
Rita Tang <doggymint@gmail.com>
2023-10-06 12:54:59 -0400

    Move all functions and states to hooks/useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
 import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import useApplicationData from './hooks/useApplicationData';
 
 import "./App.scss";
 
 const App = () => {
-  const [modalPhoto, setModalPhoto] = useState(null); // modal with a photo key will show the photo
-  const [favPhotos, setFavPhotos] = useState([]);
-
-  // Include the photo in favPhotos when it is favorited
-  const addFavPhoto = (photoId) => {
-    setFavPhotos((prev) => {
-      if (!prev.includes(photoId)) {
-        return [...prev, photoId];
-      }
-    });
-  };
-
-  // Remove a photo from favPhotos when it is unfavorited
-  const removeFavPhoto = (photoId) => {
-    setFavPhotos((prev) => {
-      const index = prev.indexOf(photoId);
-      return index >= 0 ? prev.splice(index, 1) : prev;
-    });
-  };
-
-  const handleFavButtonClick = (photoId) => {
-    // Run addFavPhoto to include the current photo when it is clicked if it's not yet in favPhotos
-    // Otherwise, run the removeFavPhoto to remove the current photo from favorited photos
-    favPhotos.includes(photoId)
-      ? removeFavPhoto(photoId)
-      : addFavPhoto(photoId);
-  };
+  const {
+    state: { favPhotos, modalPhoto },
+    setModalPhoto,
+    handleFavButtonClick,
+  } = useApplicationData();
 
   return (
     <div className="App">
---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+const useApplicationData = () => {
+  const [modalPhoto, setModalPhoto] = useState(null); // modal with a photo key will show the photo
+  const [favPhotos, setFavPhotos] = useState([]);
+
+  // Include the photo in favPhotos when it is favorited
+  const addFavPhoto = (photoId) => {
+    setFavPhotos((prev) => {
+      if (!prev.includes(photoId)) {
+        return [...prev, photoId];
+      }
+    });
+  };
+
+  // Remove a photo from favPhotos when it is unfavorited
+  const removeFavPhoto = (photoId) => {
+    setFavPhotos((prev) => {
+      const index = prev.indexOf(photoId);
+      return index >= 0 ? prev.splice(index, 1) : prev;
+    });
+  };
+
+  const handleFavButtonClick = (photoId) => {
+    // Run addFavPhoto to include the current photo when it is clicked if it's not yet in favPhotos
+    // Otherwise, run the removeFavPhoto to remove the current photo from favorited photos
+    favPhotos.includes(photoId)
+      ? removeFavPhoto(photoId)
+      : addFavPhoto(photoId);
+  };
+
+  return {
+    state: { modalPhoto, favPhotos },
+    setModalPhoto,
+    handleFavButtonClick,
+  };
+};
+
+export default useApplicationData;
