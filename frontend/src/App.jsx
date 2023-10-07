bb1551d9a6fc16db0e7e712496e0d5039ace2065
Rita Tang <doggymint@gmail.com>
2023-10-07 13:34:13 -0400

    Move photo and topic data to come from useApplicationData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,20 @@ import React from "react";
 import HomeRoute from "routes/HomeRoute";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import useApplicationData from "./hooks/useApplicationData";
-import photos from "./mocks/photos";
-import topics from "mocks/topics";
 
 import "./App.scss";
 
 const App = () => {
   const {
-    state: { favPhotos, modalPhoto },
+    state: { photoData, topicData, favPhotos, modalPhoto },
     dispatch,
   } = useApplicationData();
 
   return (
     <div className="App">
       <HomeRoute
-        photos={photos}
-        topics={topics}
+        photos={photoData}
+        topics={topicData}
         setModalPhoto={(photo) =>
           dispatch({ type: "SELECT_PHOTO", payload: photo })
         }
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,38 +1,41 @@
 import { useReducer } from "react";
+import photos from "../mocks/photos";
+import topics from "../mocks/topics";
 
 const reducer = (state, action) => {
   switch (action.type) {
-  case "FAV_PHOTO_ADDED":
-    // include the photo in favPhoto only when the current photo is not in favPhoto
-    if (!state.favPhotos.includes(action.payload)) {
-      return { ...state, favPhotos: [...state.favPhotos, action.payload] };
+    case "FAV_PHOTO_ADDED":
+      // include the photo in favPhoto only when the current photo is not in favPhoto
+      if (!state.favPhotos.includes(action.payload)) {
+        return { ...state, favPhotos: [...state.favPhotos, action.payload] };
+      }
+      return state;
+
+    case "FAV_PHOTO_REMOVED": {
+      // look for the index for the current photo
+      const index = state.favPhotos.indexOf(action.payload);
+      if (index >= 0) {
+        return { ...state, favPhotos: state.favPhotos.splice(index, 1) };
+      }
+      return state;
     }
-    return state;
 
-  case "FAV_PHOTO_REMOVED":
-    // look for the index for the current photo
-    const index = state.favPhotos.indexOf(action.payload);
-    if (index >= 0) {
-      return { ...state, favPhotos: state.favPhotos.splice(index, 1) };
-    }
-    return state;
+    case "SET_PHOTO_DATA":
+      return { ...state, photoData: action.payload };
 
-  case "SET_PHOTO_DATA":
-    return { ...state, photoData: action.payload };
+    case "SET_TOPIC_DATA":
+      return { ...state, topicData: action.payload };
 
-  case "SET_TOPIC_DATA":
-    return { ...state, topicData: action.payload };
+    case "SELECT_PHOTO":
+      if (action.payload) {
+        return { ...state, modalPhoto: action.payload };
+      }
+      return { ...state, modalPhoto: null };
 
-  case "SELECT_PHOTO":
-    if (action.payload) {
-      return { ...state, modalPhoto: action.payload };
-    }
-    return { ...state, modalPhoto: null };
-
-  default:
-    throw new Error(
-      `Tried to reduce with unsupported action type: ${action.type}`
-    );
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
   }
 };
 
@@ -40,8 +43,8 @@ const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
     favPhotos: [],
     modalPhoto: null,
-    photoData: null,
-    topicData: null,
+    photoData: [],
+    topicData: [],
   });
 
   return {
