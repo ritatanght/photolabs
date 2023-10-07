c016d0854c31034e517505d7a5df781e8f9a744d
Rita Tang <doggymint@gmail.com>
2023-10-07 13:34:46 -0400

    Add checking for arrays before mapping over

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,16 +3,18 @@ import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
 const PhotoList = (props) => {
-  const { photos,  favPhotos, ...otherProps } = props;
+  const { photos, favPhotos, ...otherProps } = props;
 
-  const photoElements = photos.map((photo) => (
-    <PhotoListItem
-      key={photo.id}
-      photo={photo}
-      isFav={!!favPhotos.includes(photo.id)}
-      {...otherProps}
-    />
-  ));
+  const photoElements =
+    Array.isArray(photos) &&
+    photos.map((photo) => (
+      <PhotoListItem
+        key={photo.id}
+        photo={photo}
+        isFav={!!favPhotos.includes(photo.id)}
+        {...otherProps}
+      />
+    ));
   return <ul className="photo-list">{photoElements}</ul>;
 };
 
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,7 +3,7 @@ import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
 const TopicList = ({topics}) => {
-  const topicElements = topics.map((topic) => (
+  const topicElements = Array.isArray(topics) && topics.map((topic) => (
     <TopicListItem key={topic.id} {...topic} />
   ));
   return <div className="top-nav-bar__topic-list">{topicElements}</div>;
