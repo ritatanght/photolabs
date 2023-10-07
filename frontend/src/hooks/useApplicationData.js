40d3f1d022a329a5e2263ba9ead27c23e112f678
Rita Tang <doggymint@gmail.com>
2023-10-07 14:04:32 -0400

    Fetch topics data from /api/topics

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,5 +1,4 @@
 import { useEffect, useReducer } from "react";
-import topics from "../mocks/topics";
 
 export const ACTIONS = {
   FAV_PHOTO_ADDED: "FAV_PHOTO_ADDED",
@@ -12,38 +11,38 @@ export const ACTIONS = {
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "FAV_PHOTO_ADDED":
-      // include the photo in favPhoto only when the current photo is not in favPhoto
-      if (!state.favPhotos.includes(action.payload)) {
-        return { ...state, favPhotos: [...state.favPhotos, action.payload] };
-      }
-      return state;
-
-    case "FAV_PHOTO_REMOVED": {
-      // look for the index for the current photo
-      const index = state.favPhotos.indexOf(action.payload);
-      if (index >= 0) {
-        return { ...state, favPhotos: state.favPhotos.splice(index, 1) };
-      }
-      return state;
+  case "FAV_PHOTO_ADDED":
+    // include the photo in favPhoto only when the current photo is not in favPhoto
+    if (!state.favPhotos.includes(action.payload)) {
+      return { ...state, favPhotos: [...state.favPhotos, action.payload] };
     }
+    return state;
 
-    case "SET_PHOTO_DATA":
-      return { ...state, photoData: action.payload };
+  case "FAV_PHOTO_REMOVED": {
+    // look for the index for the current photo
+    const index = state.favPhotos.indexOf(action.payload);
+    if (index >= 0) {
+      return { ...state, favPhotos: state.favPhotos.splice(index, 1) };
+    }
+    return state;
+  }
 
-    case "SET_TOPIC_DATA":
-      return { ...state, topicData: action.payload };
+  case "SET_PHOTO_DATA":
+    return { ...state, photoData: action.payload };
 
-    case "SELECT_PHOTO":
-      if (action.payload) {
-        return { ...state, modalPhoto: action.payload };
-      }
-      return { ...state, modalPhoto: null };
+  case "SET_TOPIC_DATA":
+    return { ...state, topicData: action.payload };
 
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
+  case "SELECT_PHOTO":
+    if (action.payload) {
+      return { ...state, modalPhoto: action.payload };
+    }
+    return { ...state, modalPhoto: null };
+
+  default:
+    throw new Error(
+      `Tried to reduce with unsupported action type: ${action.type}`
+    );
   }
 };
 
@@ -61,7 +60,11 @@ const useApplicationData = () => {
       .then((data) =>
         dispatch({ type: ACTIONS.SET_PHOTO_DATA, payload: data })
       );
-
+    fetch("/api/topics")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: ACTIONS.SET_TOPIC_DATA, payload: data })
+      );
   }, []);
 
   return {
